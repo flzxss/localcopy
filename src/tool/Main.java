6ef40e98e2b1dca1625d1838c04e3a937dd118eb
iceroot <zxcyhn@126.com>
2018-03-15 13:53:50 +0800

    localcopy

--- src/tool/Main.java
+++ src/tool/Main.java
@@ -4,23 +4,29 @@ import java.io.File;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.core.util.StrUtil;
 
 public class Main {
 
     public static void main(String[] args) {
-        FileUtil.clean(new File("F:/deploy/apache-tomcat-7.0.85/logs"));
-        FileUtil.del("F:/deploy/apache-tomcat-7.0.85/webapps/hutool-wx");
-        FileUtil.del("F:/deploy/apache-tomcat-7.0.85/webapps/hutool-wx.war");
-        FileUtil.clean(new File("F:/deploy/apache-tomcat-7.0.85/work"));
-        if(!FileUtil.exist("F:/deploy/apache-tomcat-7.0.85/webapps/hutool-wx")){
+        String tomcatPath = "F:/deploy/apache-tomcat-7.0.85";
+        String projectName = "hutool-wx";
+        String workSpace = "C:/Users/Administrator/workspace";
+        tomcatPath = StrUtil.removeSuffix(tomcatPath, "/");
+        workSpace = StrUtil.removeSuffix(workSpace, "/");
+        FileUtil.clean(new File(tomcatPath + "/logs"));
+        FileUtil.del(tomcatPath + "/webapps/hutool-wx");
+        FileUtil.del(tomcatPath + "/webapps/hutool-wx.war");
+        FileUtil.clean(new File(tomcatPath + "/work"));
+        if (!FileUtil.exist(tomcatPath + "/webapps/hutool-wx")) {
             System.out.println("文件夹已删除");
         }
-        if(!FileUtil.exist("F:/deploy/apache-tomcat-7.0.85/webapps/hutool-wx.war")){
+        if (!FileUtil.exist(tomcatPath + "/webapps/hutool-wx.war")) {
             System.out.println("war已删除");
         }
         ThreadUtil.sleep(1000);
-        String src="C:/Users/Administrator/workspace/hutool-wx/target/hutool-wx.war";
-        String dest="F:/deploy/apache-tomcat-7.0.85/webapps/hutool-wx.war";
+        String src = workSpace + "/" + projectName + "/target/" + projectName + ".war";
+        String dest = tomcatPath + "/webapps/hutool-wx.war";
         FileUtil.copy(src, dest, true);
     }
 
